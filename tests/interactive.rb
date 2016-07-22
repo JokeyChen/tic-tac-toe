53afd2292ed4b34961b57481ea427cabc5ab533f
JokeyChen <jokey.chen@gmail.com>
2016-07-22 21:27:51 +0800

    Stop the game when the board is full; add Location class

---
+++ tests/interactive.rb
@@ -0,0 +1,5 @@
+require_relative "../tic_tac_toe"
+
+g = Game.new
+# puts "Player \"#{g.playerX.name}\" with the mark \"#{g.playerX.mark}\""
+# puts "Player \"#{g.playerY.name}\" with the mark \"#{g.playerY.mark}\""
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,10 +5,6 @@ class Player
     @name = name
     @mark = mark
   end
-
-  def make_move(x, y)
-    stone = Stone.new(self)
-  end
 end
 
 class Stone
@@ -31,16 +27,20 @@ class Board
     @matrix = [upper_row, mid_row, lower_row]
   end
 
-  def valid_move?(x, y)
-    unless @matrix[y][x] == "="
+  def valid_move?(location)
+    if @matrix[location.y] == nil || @matrix[location.y][location.x] == nil
       false
     else
-      true
+      if @matrix[location.y][location.x] == "="
+        true
+      else
+        false
+      end
     end
   end
 
-  def put_stone_at(stone, x, y)
-    @matrix[y][x] = stone.sourcePlayer.mark
+  def put_stone_at(stone, location)
+    @matrix[location.y][location.x] = stone.sourcePlayer.mark
   end
 
   def display_board
@@ -48,19 +48,61 @@ class Board
       puts "#{row[0]} #{row[1]} #{row[2]}"
     end
   end
+
+  def full?
+    @matrix.each do |row|
+      row.each do |e|
+        return false if e == EMPTY
+      end
+    end
+    return true
+  end
+end
+
+class Location
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
 end
 
 class Game
   attr_reader :playerX, :playerY, :playerInTurn, :board
 
   def initialize
+    display_welcome
+    game_setup
+    game_loop
+    display_winner
+    end_game
+  end
+
+  def game_setup
     @board = Board.new
-    @playerX = Player.new("X", "X")
-    @playerY = Player.new("Y", "O")
-    start_game
+    @playerX = create_player
+    @playerY = create_player
+  end
+
+  def create_player
+    # prompt the user for player's name
+    puts "What's the name of this player?"
+    name = gets.chomp
+    # prompt the user for player's mark
+    puts "What's the mark of this player?"
+    mark = gets.chomp
+    # create the player and return
+    Player.new(name, mark)
   end
 
   def continue?
+    # stop when the board is no longer empty
+    if @board.full?
+      false
+    else
+      true
+    end
   end
 
   def switch_player
@@ -71,29 +113,69 @@ class Game
     end
   end
 
-  def start_game
+  def game_loop
     @playerInTurn = @playerX
+    while continue?
+      @board.display_board
+      parse_command
+    end
+    display_winner
+    end_game
+  end
+
+  def parse_command
+    case prompt_for_instruction
+    when "move"
+      location = prompt_for_location
+      switch_player if make_move(location)
+    when "exit"
+      end_game
+    else
+      puts "Invalid command!"
+    end
+  end
+
+  def prompt_for_instruction
+    puts "Please enter a command (move or exit):"
+    gets.chomp
+  end
+
+  def prompt_for_location
+    puts "Please enter the x coordinate:"
+    # convert the string to int then to 0-based
+    x = gets.chomp.to_i - 1
+    puts "Please enter the y coordinate:"
+    y = gets.chomp.to_i - 1
+    Location.new(x, y)
   end
 
   def end_game
+    puts "GOODBYE!!!!"
+    exit
   end
 
   def display_winner
+    # the last player in turn is the winner
+    winner = playerInTurn == playerX ? playerY : playerX
+    puts "The winner is: #{winner.name}! Congratulations!"
   end
 
   def display_welcome
+    puts "WELCOME!!!!"
   end
 
   def display_invalid_move
     puts "INVALID MOVE"
   end
 
-  def make_move(x, y)
-    if @board.valid_move?(x, y)
+  def make_move(location)
+    if @board.valid_move?(location)
       stone = Stone.new(playerInTurn)
-      @board.put_stone_at(stone, x, y)
+      @board.put_stone_at(stone, location)
+      true
     else
-      display_error
+      display_invalid_move
+      false
     end
   end
 
